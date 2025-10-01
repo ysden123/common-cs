d1e4d89157a7c5b987a40e510447a79009a6f93a
Yuriy Stul <ysden123@gmail.com>
2025-10-01 15:59:51 +0300

    Utils added

---
+++ common/Utils.cs
@@ -0,0 +1,15 @@
+﻿namespace YSCommon
+{
+    public class Utils
+    {
+        /// <summary>
+        /// Outputs the current thread name and a message to the console.
+        /// </summary>
+        /// <param name="message">A message to write on console</param>
+        public static void Trace(string message)
+        {
+            var t = DateTime.Now.ToString("HH:mm:ss:fff");
+            Console.WriteLine($"[{Thread.CurrentThread.Name}, {t}]: {message}");
+        }
+    }
+}
