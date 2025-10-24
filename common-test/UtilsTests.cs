2256ece9dad28ee680be65cd9bb0238f11f2f77a
Yuriy Stul <ysden123@gmail.com>
2025-10-24 09:58:41 +0300

    Getting folder according assembly name

---
+++ common-test/UtilsTests.cs
@@ -0,0 +1,18 @@
+﻿using YSCommon;
+
+namespace common_test;
+
+public class UtilsTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var folder = Utils.GetAssemblyFolderInLocalData();
+        Assert.That(folder, Does.EndWith(@"\AppData\Local\common-test"));
+    }
+}
--- common/Utils.cs
+++ common/Utils.cs
@@ -1,4 +1,6 @@
-﻿namespace YSCommon
+﻿using System.Reflection;
+
+namespace YSCommon
 {
     public class Utils
     {
@@ -10,6 +12,20 @@
         {
             var t = DateTime.Now.ToString("HH:mm:ss:fff");
             Console.WriteLine($"[{Thread.CurrentThread.Name}, {t}]: {message}");
+        }
+
+        /// <summary>
+        /// Builds and returns the path to a folder in LocalApplicationData named after the executing assembly.   
+        /// </summary>
+        /// <returns>The path to a folder in LocalApplicationData named after the executing assembly</returns>
+        /// <exception cref="Exception">If can't get an Assembly the Assembly name</exception>
+        public static string GetAssemblyFolderInLocalData()
+        {
+            //Assembly assembly = Assembly.GetExecutingAssembly() ?? throw new Exception("Can't get excuting assembly!");
+            Assembly assembly = Assembly.GetCallingAssembly() ?? throw new Exception("Can't get excuting assembly!");
+            string assemblyName = assembly.GetName().Name ?? throw new Exception("Can't get assembly name!");
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), assemblyName);
+            return folder;
         }
     }
 }
